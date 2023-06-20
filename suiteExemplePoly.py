f0b57c77648c1ee7701eabebe2fa29b984151fa3
rbabari <r.babari@gmail.com>
2023-06-20 11:37:57 -0400

    Exemple polymorphime : suite Employe et enseignant ...

--- suiteExemplePoly.py
+++ suiteExemplePoly.py
@@ -19,7 +19,7 @@ class Personne:
         self.age = p_age
 
     def presenteToi(self):
-        print('Je suis ', self.prenom, 'et j ai', self.age)
+        print('Personne [Je suis ', self.prenom, 'et j ai', self.age, ']')
 
 #class Etudiant hérite de Personne ::
 class Etudiant(Personne):
@@ -31,7 +31,29 @@ class Etudiant(Personne):
         self.AnneeGraduation =p_AnneeGraduation
 
     def presenteToi(self):
-        print('Je suis ', self.prenom, 'et suis dans le gr : ', self.Groupe)
+        print('Etudiant [ Je suis ', self.prenom, 'et suis dans le gr : ', self.Groupe, ']')
+
+#class Etudiant hérite de Personne ::
+class Emplyee(Personne):
+    # constructeur en utilisant un objet de type Personne (parent)
+    def __init__(self, p_personne, p_numEmployee, p_service, p_bureau):
+        super().__init__(p_personne.nom, p_personne.prenom, p_personne.age)
+        self.numEmployee = p_numEmployee
+        self.service = p_service
+        self.bureau =p_bureau
+
+    def presenteToi(self):
+        print('Emplyee [ Je suis ', self.prenom, 'et suis dans le service : ', self.service, ']')
+
+
+class Enseignant(Emplyee):
+    def __init__(self, p_employee, p_departement):
+        super().__init__(Personne(p_employee.nom, p_employee.prenom, p_employee.age), p_employee.numEmployee, p_employee.service, p_employee.bureau)
+        self.departement = p_departement
+
+    def presenteToi(self):
+        print('Enseignant [ Je suis ', self.prenom, 'et suis dans le departemnt : ', self.departement, ']')
+
 
 """
   # constructeur V2 en utilisant un objet de type Personne (parent)
@@ -53,12 +75,16 @@ liste = []
 liste.append(Personne('babari', 'raouf', 31))
 liste.append(Personne('bachir', 'Fikry', 25))
 liste.append(Personne('Nabil', 'Agsous', 40))
-
 E1 = Etudiant(liste[1], 1234567, 1253, 2024)
 E2 = Etudiant(Personne('Nabil', 'Agsous', 40), 1234567, 1253, 2024)
+Emp1 = Emplyee(Personne('babari', 'raouf', 26),  1234, 'Informatique', 'H12')
+Ens1 = Enseignant(Emp1, 'dep Informatique')
+
+
 liste.append(E1)
 liste.append(E2)
-
+liste.append(Emp1)
+liste.append(Ens1)
 # constructeur V2 : E3 = Etudiant('Nabil', 'Agsous', 40, 1234567, 1253, 2024)
 
 # Les objets de la même classe se comportemt souvent de la même manière ...
